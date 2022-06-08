399f74e7dfeb3f7014b0d599ecd0f1ae204dfeae
sla6ak <86052868+sla6ak@users.noreply.github.com>
2022-06-08 08:28:35 +0000

    Deploying to gh-pages from @ sla6ak/app-world-future@6a4c2150148def76f228018c7fb2240919507ec9 🚀

--- server/pages/_app.js
+++ server/pages/_app.js
@@ -16,11 +16,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
 
 
-function MyApp({ Component , pageProps  }) {
-    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
-        ...pageProps
+const MyApp = ({ Component , pageProps  })=>{
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+            ...pageProps
+        })
     });
-}
+};
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
 
 
